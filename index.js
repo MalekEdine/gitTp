45c0ac5af78c77576ca7969e4090819574b95762
MalekEdine <m.edine.ec@gmail.com>
2022-11-15 12:15:54 +0100

    Ajout derniere fonctionnalite

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 console.log('hello');
 console.log('World');
 console.log('ça va');
+console.log('hello');
+console.log('World');
+console.log('ça va');
+console.log('hello');
+console.log('World');
+console.log('ça va');
 
+
+// function logIn = () => {
+//     return console.log('connected')
+// }
+
+// function logIn = () => {
+//     return console.log('connected')
+// }
 
 // function logIn = () => {
 //     return console.log('connected')
